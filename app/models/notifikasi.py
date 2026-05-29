0ffbe7854fd11b50e6816c9eaa2a06343e2b640b
aidilprmdta <sekaiiprojectmv@gmail.com>
2026-05-29 15:55:18 +0700

    🔥 feat – menambahkan BE notifikasi

--- app/models/notifikasi.py
+++ app/models/notifikasi.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, TIMESTAMP
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.config.database import Base
+
 
 class Notifikasi(Base):
     __tablename__ = "notifikasi"
@@ -8,6 +10,11 @@ class Notifikasi(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     pesan = Column(String(255), nullable=False)
-    # Default False = belum dibaca
     is_read = Column(Boolean, default=False, nullable=False)
     created_at = Column(TIMESTAMP, server_default=func.now())
+
+    user = relationship(
+        "User",
+        back_populates="notifikasi_list",
+        foreign_keys=[user_id],
+    )
